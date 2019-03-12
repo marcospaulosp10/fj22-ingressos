0d36f7e04ad152b1f246cf4ea2576175027b8bff
Marcos Paulo <marcos@cnj.jus.br>
2019-03-12 10:07:58 -0300

    Correção do autowired no sessao controller

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,10 +14,13 @@ import br.com.caelum.ingresso.dao.SalaDao;
 @Controller
 public class SessaoController {
 
+	@Autowired
 	private FilmeDao filmeDao;
+	
+	@Autowired
 	private SalaDao salaDao;
 
-    @GetMapping({"/admin/sessao"})
+    @GetMapping({"admin/sessao"})
     public ModelAndView form(Integer salaId){
         ModelAndView mv = new ModelAndView("sessao/sessao");
     
