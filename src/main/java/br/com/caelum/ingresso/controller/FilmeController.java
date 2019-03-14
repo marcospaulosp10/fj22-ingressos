ed08173334a54a59feab8892ed1e23b28a9366c2
Marcos Paulo <marcos@cnj.jus.br>
2019-03-14 11:10:27 -0300

    Implementação da RESTapi do omdb para buscar informações de detalhes dos filmes

--- src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
+++ src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
@@ -18,8 +18,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.DetalhesDoFilme;
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.rest.OmdbClient;
 
 /**
  * Created by nando on 03/03/17.
@@ -33,6 +35,9 @@ public class FilmeController {
     
     @Autowired
     private SessaoDao sessaoDao;
+    
+    @Autowired
+    private OmdbClient client;
 
 
     @GetMapping({"/admin/filme", "/admin/filme/{id}"})
@@ -62,7 +67,11 @@ public class FilmeController {
     	ModelAndView mv = new ModelAndView("filme/detalhe");
     	Filme filme = filmeDao.findOne(id);
     	List<Sessao> sessoes = sessaoDao.buscaSessoesDoFilme(filme);
+    	
+    	Optional<DetalhesDoFilme> detalhesDoFilme = client.request(filme);
+
     	mv.addObject("sessoes", sessoes);
+    	mv.addObject("detalhes", detalhesDoFilme.orElse(new DetalhesDoFilme()));
         return mv;
     }
 
