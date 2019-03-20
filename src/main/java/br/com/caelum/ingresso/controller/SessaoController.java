ecae762f9052376a62849f0ebafb575b786c6cb6
Marcos Paulo <marcos@cnj.jus.br>
2019-03-20 11:24:54 -0300

    Criando permissões para os usuários e tela de login

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -16,6 +16,7 @@ import org.springframework.web.servlet.ModelAndView;
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SalaDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.Carrinho;
 import br.com.caelum.ingresso.model.ImagemCapa;
 import br.com.caelum.ingresso.model.Sessao;
 import br.com.caelum.ingresso.model.TipoDeIngresso;
@@ -39,6 +40,8 @@ public class SessaoController {
 	
 	@Autowired
 	private OmdbClient client;
+	
+	private Carrinho carrinho;
 
     @GetMapping("admin/sessao")
     public ModelAndView form(Integer salaId, SessaoForm form){
@@ -64,6 +67,7 @@ public class SessaoController {
     	Optional<ImagemCapa> imagemCapa = client.request(sessao.getFilme(), ImagemCapa.class);
 
         mv.addObject("sessao", sessao);
+        mv.addObject("carrinho", carrinho);
         mv.addObject("imagemCapa", imagemCapa.orElse(new ImagemCapa()));
         mv.addObject("tiposDeIngressos", TipoDeIngresso.values());
         
---
+++ src/main/java/br/com/caelum/ingresso/model/Compra.java
@@ -0,0 +1,27 @@
+package br.com.caelum.ingresso.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Compra {
+	@Id
+	@GeneratedValue
+	private	Long id;
+	
+	@OneToMany(cascade	=	CascadeType.PERSIST)
+	List<Ingresso>	ingressos	=	new	ArrayList<>();
+	
+	public	Compra() {
+	}
+	public	Compra(List<Ingresso>	ingressos) {
+					this.ingressos	=	ingressos;
+	}
+	
+}
